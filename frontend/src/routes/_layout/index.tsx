03a52fc29337f89bb22f16740b68132cb7079061
jiyeoniing <apple20011215@gmail.com>
2024-07-24 22:30:06 +0900

    Feat : header layout complete

--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -1,15 +1,19 @@
 import {createFileRoute} from '@tanstack/react-router';
 
-import GeneralButton from '../../components/Buttons/GeneralButton.tsx';
+import GeneralButton from '../../components/Buttons/GeneralButton';
+import Header from '../../components/Header';
 
 export const Route = createFileRoute('/_layout/')({
   component: () => (
     <div>
-      <GeneralButton
-        buttonStyle={{style: 'flexible', type: 'outlined'}}
-        disabled
-      >
-        Button
+      <Header
+        location="내위치"
+        infoIcon="내정보"
+        searchIcon="통합검색"
+        alramIcon="알람"
+      />
+      <GeneralButton buttonStyle={{style: 'primary', size: 'medium'}}>
+        텍스트
       </GeneralButton>
     </div>
   ),
