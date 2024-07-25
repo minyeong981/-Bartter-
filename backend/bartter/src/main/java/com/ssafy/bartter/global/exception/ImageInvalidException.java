975b2c773e4f2c0debe46be45b5d45ff3aad1682
김용수 <akys159357@naver.com>
2024-07-25 14:04:05 +0900

    Refactor: Modify S3UploadService Exception

--- backend/bartter/src/main/java/com/ssafy/bartter/global/exception/ImageInvalidException.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.bartter.global.exception;
-
-public class ImageInvalidException extends RuntimeException{
-
-    public ImageInvalidException(String message) {
-        super(message);
-    }
-}
