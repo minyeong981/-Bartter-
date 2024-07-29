e1d45f382e469d1de55339a5b85bd1bdc0671895
BHyeonKim <rlaqhguse@gmail.com>
2024-07-29 13:53:34 +0900

    Feat: 서버로 로그인 요청

--- frontend/src/routes/_layout/login/index.tsx
+++ frontend/src/routes/_layout/login/index.tsx
@@ -1,32 +1,72 @@
 import {createFileRoute} from '@tanstack/react-router';
 import classnames from 'classnames/bind';
+import type {ChangeEvent} from 'react';
 
 import {ImageLogo} from '@/assets/image';
 import GeneralButton from '@/components/Buttons/GeneralButton.tsx';
+import LinkButton from '@/components/Buttons/LinkButton.tsx';
 import HeaderWithBackButton from '@/components/Header/HeaderWithBackButton.tsx';
 import GeneralInput from '@/components/Inputs/GeneralInput.tsx';
+import useLoginForm from '@/hooks/useLoginForm.tsx';
+import barter from '@/services/barter.ts';
+import {PASSWORD_PATTERN, USERID_PATTERN} from '@/util/validation.ts';
 
 import styles from './login.module.scss';
 
 const cx = classnames.bind(styles);
 
 export const Route = createFileRoute('/_layout/login/')({
-  component: () => (
+  component: LoginPage,
+});
+
+function LoginPage() {
+  const {form, handleUsernameChange, handlePasswordChange} = useLoginForm();
+  const isValid =
+    form.username.match(USERID_PATTERN) &&
+    form.password.match(PASSWORD_PATTERN);
+
+  function handleUsername(e: ChangeEvent<HTMLInputElement>) {
+    handleUsernameChange(e.currentTarget.value);
+  }
+
+  function handlePassword(e: ChangeEvent<HTMLInputElement>) {
+    handlePasswordChange(e.currentTarget.value);
+  }
+
+  async function handleLogin() {
+    await barter.login(form);
+  }
+
+  return (
     <div className={cx('login')}>
       <HeaderWithBackButton />
       <div className={cx('logo-container')}>
         <img src={ImageLogo} alt="logo image" />
       </div>
       <form className={cx('form-container')}>
-        <GeneralInput placeholder="아이디" />
-        <GeneralInput placeholder="비밀번호" />
-        <GeneralButton buttonStyle={{style: 'primary', size: 'large'}}>
+        <GeneralInput
+          placeholder="아이디"
+          onChange={handleUsername}
+          value={form.username}
+          pattern={USERID_PATTERN.source}
+        />
+        <GeneralInput
+          placeholder="비밀번호"
+          onChange={handlePassword}
+          value={form.password}
+          pattern={USERID_PATTERN.source}
+        />
+        <GeneralButton
+          buttonStyle={{style: 'primary', size: 'large'}}
+          onClick={handleLogin}
+          disabled={!isValid}
+        >
           로그인
         </GeneralButton>
-        <GeneralButton buttonStyle={{style: 'mono', size: 'large'}}>
+        <LinkButton buttonStyle={{style: 'mono', size: 'large'}} to="/signup">
           회원가입
-        </GeneralButton>
+        </LinkButton>
       </form>
     </div>
-  ),
-});
+  );
+}
