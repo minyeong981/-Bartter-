540ba39f4125f3995c27b788bf30adbf733197b5
BHyeonKim <rlaqhguse@gmail.com>
2024-07-29 19:59:19 +0900

    Feat: 동네모임 타입 추가

--- frontend/src/types/barter.d.ts
+++ frontend/src/types/barter.d.ts
@@ -10,14 +10,6 @@ type Longitude = number;
 type PhoneNumber = string;
 type Email = string;
 
-interface BarterResponse<T> {
-  isSuccess: boolean;
-  code: number;
-  message: string;
-  data: T;
-  errors: string[];
-}
-
 interface SignupForm {
   username: Username;
   password: Password;
@@ -35,7 +27,82 @@ interface LoginForm {
   password: Password;
 }
 
+type ImageId = number;
+type ImageUrl = string;
+type ImageOrder = number;
+
+interface SimpleImage {
+  imageId: ImageId;
+  imageUrl: ImageUrl;
+  imageOrder: ImageOrder;
+}
+
+type LocationId = number;
+type LocationName = string;
+
+interface SimpleLocation {
+  locationId: LocationId;
+  locationName: LocationName;
+}
+
+type CommunityPostId = number;
+type Title = string;
+type LikeCount = number;
+type CommentCount = number;
+type Image = string | null;
+
+interface CommunityPost {
+  communityPostId: CommunityPostId;
+  user: SimpleUser;
+  location: SimpleLocation;
+  title: Title;
+  content: Content;
+  likeCount: LikeCount;
+  commentList: Comment[];
+  imageList: SimpleImage[];
+  createdAt: CreatedAt;
+}
+
+interface SimpleCommunityPost {
+  communityPostId: CommunityPostId;
+  location: SimpleLocation;
+  title: Title;
+  content: Content;
+  likeCount: LikeCount;
+  commentCount: CommentCount;
+  image: Image;
+  createdAt: CreatedAt;
+}
+
+type UserId = number;
+type ProfileImage = string;
+
+interface SimpleUser {
+  userId: UserId;
+  nickname: Nickname;
+  profileImage: ProfileImage;
+}
+
+type CommentId = number;
+type Content = string;
+type CreatedAt = string;
+
+interface Comment {
+  commentId: CommentId;
+  user: SimpleUser;
+  content: Content;
+  created_at: CreatedAt;
+}
+
+interface BarterResponse<T> {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+  data: T;
+  errors: string[];
+}
+
 type SignupResponse = BarterResponse<null>;
 type LoginResponse = BarterResponse<null>;
 type ReIssueResponse = BarterResponse<null>;
-type LogoutResponse = BarterResponse<null>
+type LogoutResponse = BarterResponse<null>;
