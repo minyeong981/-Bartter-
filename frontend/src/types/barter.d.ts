69b5faa6f2a85fe75ca53583af10eaee4139d878
BHyeonKim <rlaqhguse@gmail.com>
2024-07-29 11:42:33 +0900

    Feat: 회원 기능 서비스 정의

--- frontend/src/types/barter.d.ts
+++ frontend/src/types/barter.d.ts
@@ -9,3 +9,33 @@ type Latitude = number;
 type Longitude = number;
 type PhoneNumber = string;
 type Email = string;
+
+interface BarterResponse<T> {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+  data: T;
+  errors: string[];
+}
+
+interface SignupForm {
+  username: Username;
+  password: Password;
+  nickname: Nickname;
+  birth: Birth;
+  gender: Gender;
+  latitude: Latitude;
+  longitude: Longitude;
+  phone: PhoneNumber;
+  email?: Email;
+}
+
+interface LoginForm {
+  username: Username;
+  password: Password;
+}
+
+type SignupResponse = BarterResponse<null>;
+type LoginResponse = BarterResponse<null>;
+type ReIssueResponse = BarterResponse<null>;
+type LogoutResponse = BarterResponse<null>
