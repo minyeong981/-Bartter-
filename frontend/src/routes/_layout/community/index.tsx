6fc913727d2adfa2e237242a90a50bb38ed3e44e
jiyeoniing <apple20011215@gmail.com>
2024-07-30 01:48:18 +0900

    Feat: 동네모임 게시글 생성 구현

--- frontend/src/routes/_layout/community/index.tsx
+++ frontend/src/routes/_layout/community/index.tsx
@@ -1,73 +1,32 @@
 import {createFileRoute} from '@tanstack/react-router';
 import {useState} from 'react';
 
-import CommunityImage from '@/assets/image/동네모임1.png';
 import GeneralButton from '@/components/Buttons/LinkButton';
 import PostList from '@/components/Community/PostList';
 import HeaderWithLabelAndButtons from '@/components/Header/HeaderWithLabelAndButtons';
 import TwoButton from '@/components/TwoButton/TwoButton';
+import useCommunityStore from '@/store/communityStore';
 
 import styles from './community.module.scss';
 
-const allPosts = [
-  {
-    comunityPostId: 1,
-    locationName: '서울 강남구',
-    title: '동네 마라톤 대회1',
-    content: '함께 달려요! 초보자도 환영합니다.',
-    likeCount: 12,
-    commentCount: 3,
-    image: CommunityImage,
-    createdAt: '2024-07-25 오후 5:00',
-  },
-  {
-    comunityPostId: 2,
-    locationName: '서울 마포구',
-    title: '동네 마라톤 대회2',
-    content: '함께 달려요! 초보자도 환영합니다.',
-    likeCount: 12,
-    commentCount: 3,
-    image: CommunityImage,
-    createdAt: '2024-07-25 오후 5:00',
-  },
-  {
-    comunityPostId: 3,
-    locationName: '광주 장덕동',
-    title: '동네 마라톤 대회3',
-    content: '함께 달려요! 초보자도 환영합니다.',
-    likeCount: 12,
-    commentCount: 3,
-    image: CommunityImage,
-    createdAt: '2024-07-25 오후 5:00',
-  },
-];
-
-const neighborPosts = [
-  {
-    comunityPostId: 4,
-    locationName: '광주 장덕동',
-    title: '동네 마라톤 대회4',
-    content: '함께 달려요! 초보자도 환영합니다.',
-    likeCount: 12,
-    commentCount: 3,
-    image: CommunityImage,
-    createdAt: '2024-07-25 오후 5:00',
-  },
-];
 
 export const Route = createFileRoute('/_layout/community/')({
   component: Community,
 });
 
 export default function Community() {
+
+  const posts = useCommunityStore(state => state.posts);
+  console.log(posts)
+
   const [activeComponent, setActiveComponent] = useState<string>('전체글');
 
   const renderComponent = () => {
     switch (activeComponent) {
       case '전체글':
-        return <PostList posts={allPosts} />;
+        return <PostList posts={posts} />;
       case '동네글':
-        return <PostList posts={neighborPosts} />;
+        return <PostList posts={posts} />;
       default:
         return null;
     }
