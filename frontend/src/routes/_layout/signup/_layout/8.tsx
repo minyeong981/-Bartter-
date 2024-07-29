32116f52c8c1a27b8c1c2718c46b2b4b366b84fa
BHyeonKim <rlaqhguse@gmail.com>
2024-07-29 13:52:15 +0900

    Feat: 서버로 회원가입 요청

--- frontend/src/routes/_layout/signup/_layout/8.tsx
+++ frontend/src/routes/_layout/signup/_layout/8.tsx
@@ -3,6 +3,7 @@ import classnames from 'classnames/bind';
 
 import GeneralButton from '@/components/Buttons/GeneralButton.tsx';
 import Heading from '@/components/Heading';
+import barter from '@/services/barter.ts';
 import useSignupStore from '@/store/signupStore.ts';
 import {getPosition} from '@/util/geolocation.ts';
 
@@ -17,16 +18,40 @@ export const Route = createFileRoute('/_layout/signup/_layout/8')({
 function GetLocationPage() {
   const navigate = useNavigate({from: '/signup/1'});
   const setCoordinate = useSignupStore(state => state.setCoordinate);
+  const resetSignupForm = useSignupStore(state => state.resetSignupForm);
+  const {
+    nickname,
+    username,
+    password,
+    gender,
+    phone,
+    birth,
+    email,
+    latitude,
+    longitude,
+  } = useSignupStore(state => state);
 
-  async function handleCooridnate() {
+  async function handleSignup() {
     try {
       const {coords} = await getPosition();
       setCoordinate(coords);
+      await barter.signup({
+        gender: gender!,
+        birth,
+        latitude: latitude!,
+        longitude: longitude!,
+        email,
+        nickname,
+        username,
+        password,
+        phone,
+      });
+      resetSignupForm();
       navigate({to: '/signup/9'});
       return;
     } catch (e) {
       console.error(e);
-      alert('위치 정보를 가져오는데 실패했습니다.');
+      alert('회원가입을 하는데 실패했습니다.');
     }
   }
 
@@ -35,15 +60,15 @@ function GetLocationPage() {
       <div className={cx('headingContainer')}>
         <Heading>
           농부님의
-          <br />
+          <br/>
           위치를 등록해주세요
         </Heading>
       </div>
-      <div className={cx('inputContainer')} />
+      <div className={cx('inputContainer')}/>
       <div className={cx('buttonContainer')}>
         <GeneralButton
           buttonStyle={{style: 'primary', size: 'large'}}
-          onClick={handleCooridnate}
+          onClick={handleSignup}
         >
           위치 등록하기
         </GeneralButton>
