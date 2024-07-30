956c35a663ff6ee7e6c2e620754af43a2643f47a
유지연 <apple20011215@gmail.com>
2024-07-30 17:52:48 +0900

    Feat:동네모임 전체적 구현 & 통합검색 컴포넌트 구현

---
+++ frontend/src/components/Search/SearchBar.tsx
@@ -0,0 +1,65 @@
+
+import type { ChangeEvent, KeyboardEvent} from 'react';
+import {useState } from 'react';
+
+import searchIcon from '@/assets/image/searchIcon.png';
+import xIcon from '@/assets/image/xIcon.png';
+import HeaderWithSearchAndBackButton from '@/components/Header/HeaderWithSearchandBackButton';
+import stylesInput from '@/components/Search/Search.module.scss';
+
+import styles from './SearchBar.module.scss';
+
+interface SearchBarProps {
+  query: string;
+  onSearch: (searchTerm: string) => void;
+}
+
+
+export default function SearchBar({ query, onSearch} : SearchBarProps) {
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    onSearch(event.target.value);
+  }
+
+  function handleClear() {
+    onSearch('');
+  }
+
+  function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      if (query.trim() !== '') {
+        onSearch(event.currentTarget.value);
+        // setSearchInput('');
+      }
+    }
+  }
+
+
+  return (
+  
+    <div className={styles.header}>
+      <HeaderWithSearchAndBackButton
+        label={
+          <div className={styles.inputContainer}>
+            <div className={stylesInput.searchBar}>
+              <input
+                type="text"
+                placeholder="밭터 통합검색"
+                value={query}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+              />
+              <button className={stylesInput.clearButton} onClick={handleClear}>
+                <img src={query ? xIcon : searchIcon} alt="Clear/Search" />
+              </button>
+            </div>
+          </div>
+        }
+      />
+
+    </div>
+
+
+  );
+}
+
--- frontend/src/routes/_layout/search/index.tsx
+++ frontend/src/routes/_layout/search/index.tsx
@@ -1,13 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router';
 import type { ChangeEvent, KeyboardEvent} from 'react';
 import {useState } from 'react';
-import { FaSearch } from 'react-icons/fa';
 
-import searchIcon from '@/assets/image/searchIcon.png';
-import xIcon from '@/assets/image/xIcon.png';
-import HeaderWithSearchAndBackButton from '@/components/Header/HeaderWithSearchandBackButton';
 import RecentSearch from '@/components/Search/RecentSearch';
-import stylesInput from '@/components/Search/Search.module.scss';
+import SearchBar from '@/components/Search/SearchBar';
 import SearchResult from '@/components/Search/SearchResult';
 import SearchSuggestion from '@/components/Search/SearchSuggestions';
 
@@ -17,65 +13,25 @@ import styles from './search.module.scss';
 
 export default function Search() {
   const [ query, setQuery] = useState<string>('');
-  const [recentSearch, setRecentSearch] = useState<string[]>(['오이고추', '고구마', '무']);
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-  const [results, setResults] = useState<Result[]>([]);
+  // const [recentSearch, setRecentSearch] = useState<string[]>(['오이고추', '고구마', '무']);
+  // const [suggestions, setSuggestions] = useState<string[]>([]);
+  // const [results, setResults] = useState<Result[]>([]);
 
 
-  // const [searchInput, setSearchInput] = useState<string>('');
-  // const [searchList, setSearchList] = useState<string[]>(initialSearch);
-
-  // function handleChange(event: ChangeEvent<HTMLInputElement>) {
-  //   setSearchInput(event.target.value);
-  // }
-
-  // function handleClear() {
-  //   setSearchInput('');
-  // }
-
-  
-
-  // function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
-  //   if (event.key === 'Enter') {
-  //     if (searchInput.trim() !== '') {
-  //       setSearchList([...searchList, searchInput]);
-  //       setSearchInput('');
-  //     }
-  //   }
-  // }
+  function handleSearch(searchTerm : string) {
+    setQuery(searchTerm)
+  }
 
   return (
   
     <div className={styles.header}>
-      <HeaderWithSearchAndBackButton
-        label={
-          <div className={styles.inputContainer}>
-            <div className={stylesInput.searchBar}>
-              <input
-                type="text"
-                placeholder="밭터 통합검색"
-                value={searchInput}
-                onChange={handleChange}
-                onKeyDown={handleKeyDown}
-              />
-              <button className={stylesInput.clearButton} onClick={handleClear}>
-                <img src={searchInput ? xIcon : searchIcon} alt="Clear/Search" />
-              </button>
-            </div>
-          </div>
-        }
-      />
-{/* 
-      <div className={styles.searchList}>
-        {searchList.map((search, searchIndex) => (
-          <div key={searchIndex} className={styles.searchBox}>
-            <FaSearch className={styles.searchIcon} /> {search}
-          </div>
-        ))}
-      </div> */}
+
 
       <div>
-       {query === '' && <RecentSearch search={recentSearch} onSearch={handleSearch}/> }
+        <SearchBar query ={query} onSearch={handleSearch} />
+       {/* {query === '' && <RecentSearch search={recentSearch} onSearch={handleSearch}/> }
+       {query !== '' && results.length === 0 && <SearchSuggestion query={query} suggestions={suggestions} onSearch={handleSearch} />}
+       {results.length > 0 && <SearchResult results={results} />} */}
       </div>
 
 
