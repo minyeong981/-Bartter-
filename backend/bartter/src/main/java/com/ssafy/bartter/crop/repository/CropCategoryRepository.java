c6a9158e72775c31b6b707be1ab82a2b0b453857
garamgim <garamgim@gmail.com>
2024-07-31 15:01:13 +0900

    Feat: 농작물 카테고리 이름으로 검색 기능

--- backend/bartter/src/main/java/com/ssafy/bartter/crop/repository/CropCategoryRepository.java
+++ backend/bartter/src/main/java/com/ssafy/bartter/crop/repository/CropCategoryRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.bartter.crop.repository;
 import com.ssafy.bartter.crop.entity.CropCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CropCategoryRepository extends JpaRepository<CropCategory, Integer> {
+    List<CropCategory> findAllByNameContaining(String name);
 }
