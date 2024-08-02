fd5399dc100217a7ceabdd788aa9eaac9412d9d3
BHyeonKim <rlaqhguse@gmail.com>
2024-08-02 10:45:56 +0900

    Feat: 플로팅 버튼 추가

--- frontend/src/routes/_layout/trade/_layout.tsx
+++ frontend/src/routes/_layout/trade/_layout.tsx
@@ -1,6 +1,7 @@
 import {createFileRoute, Outlet} from '@tanstack/react-router';
 import classnames from 'classnames/bind';
 
+import FloatingButton from "@/components/Buttons/FloatingButton";
 import HeaderWithLabelAndButtons from '@/components/Header/HeaderWithLabelAndButtons.tsx';
 import Navigation from '@/components/Navigation';
 
@@ -15,9 +16,10 @@ export const Route = createFileRoute('/_layout/trade/_layout')({
 function Trade() {
   return (
     <div className={cx('trade')}>
-      <HeaderWithLabelAndButtons label="장덕동" />
-      <Outlet />
-      <Navigation />
+      <HeaderWithLabelAndButtons label="장덕동"/>
+      <Outlet/>
+      <FloatingButton>+ 글작성하기</FloatingButton>
+      <Navigation/>
     </div>
   );
 }
