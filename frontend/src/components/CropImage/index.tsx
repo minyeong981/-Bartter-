9b9cb954f9fd0f5ee63d192c19047dfb13ce1f39
BHyeonKim <rlaqhguse@gmail.com>
2024-08-02 15:59:36 +0900

    Feat: 크롭이미지 컴포넌트 추가

---
+++ frontend/src/components/CropImage/index.tsx
@@ -0,0 +1,20 @@
+import classnames from 'classnames/bind';
+
+import styles from './cropImage.module.scss';
+
+interface CropImageProps {
+  imgSrc: string;
+  label: string;
+}
+
+const cx = classnames.bind(styles);
+
+export default function CropImage({imgSrc, label}: CropImageProps) {
+  const id = `${label} image`;
+  return (
+    <div className={cx('cropImage')}>
+      <img id={id} src={imgSrc} alt={label} />
+      <label htmlFor={id}>{label}</label>
+    </div>
+  );
+}
