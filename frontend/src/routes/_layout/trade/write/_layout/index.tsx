f6493ede27fbfb7e34a79fa4f18293fe60d411cd
BHyeonKim <rlaqhguse@gmail.com>
2024-08-03 19:53:20 +0900

    Feat: 물물교환 작성 페이지 추가

--- frontend/src/routes/_layout/trade/write/_layout/index.tsx
+++ frontend/src/routes/_layout/trade/write/_layout/index.tsx
@@ -2,9 +2,11 @@ import {createFileRoute} from '@tanstack/react-router';
 import classnames from 'classnames/bind';
 
 import {ImageLettuce} from '@/assets/image';
-import SelectCropButton from '@/components/Buttons/SelectCropButton';
+import GeneralButton from '@/components/Buttons/GeneralButton.tsx';
+import LabeledSelectCropButton from '@/components/Buttons/SelectCropButton/LabeledSelectCropButton.tsx';
 import CropImage from '@/components/CropImage';
 import CheckboxInput from '@/components/Inputs/CheckboxInput.tsx';
+import LabeledImageInput from '@/components/Inputs/LabeledImageInput.tsx';
 import LabeledInput from '@/components/Inputs/LabeledInput.tsx';
 import LabeledTextAreaInput from '@/components/Inputs/LabeledTextAreaInput.tsx';
 
@@ -19,11 +21,34 @@ export const Route = createFileRoute('/_layout/trade/write/_layout/')({
 function WritePage() {
   return (
     <div className={cx('writePage')}>
-      <CropImage imgSrc={ImageLettuce} label="상추" />
-      <LabeledInput label="제목" placeholder="제목을 입력하세요" />
-      <CheckboxInput label="나눔인가요?" />
-      <SelectCropButton selectedCrops={['상추', '고추', '바나나', '상추']} />
-      <LabeledTextAreaInput label="내용" placeholder="내용을 입력하세요" />
+      <div className={cx('inputContainer')}>
+        <CropImage imgSrc={ImageLettuce} label="상추" />
+        <LabeledInput label="제목" placeholder="제목을 입력하세요" />
+        <CheckboxInput label="나눔인가요?" />
+        <LabeledSelectCropButton
+          label="주고 싶은 작물"
+          selectedCrops={['상추', '고추', '바나나', '상추']}
+        />
+        <LabeledSelectCropButton
+          label="받고 싶은 작물"
+          selectedCrops={['상추', '고추', '바나나', '상추']}
+        />
+
+        <LabeledTextAreaInput label="내용" placeholder="내용을 입력하세요" />
+        <LabeledImageInput
+          onImageChange={() => null}
+          maxImages={3}
+          label="사진"
+        />
+        <GeneralButton buttonStyle={{style: 'outlined', size: 'large'}}>
+          위치 등록
+        </GeneralButton>
+      </div>
+      <div className={cx('buttonContainer')}>
+        <GeneralButton buttonStyle={{style: 'primary', size: 'large'}}>
+          작성 완료
+        </GeneralButton>
+      </div>
     </div>
   );
 }
