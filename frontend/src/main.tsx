994b920f3ca233f9607e6f5271294a77dc0f84b7
BHyeonKim <rlaqhguse@gmail.com>
2024-08-05 04:03:14 +0900

    Chore: 리액트 쿼리 및 데브툴 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import './styles/index.scss';
 
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {createRouter, RouterProvider} from '@tanstack/react-router';
 import {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
@@ -7,8 +8,17 @@ import ReactDOM from 'react-dom/client';
 // Import the generated route tree
 import {routeTree} from './routeTree.gen.ts';
 
+const queryClient = new QueryClient();
+
 // Create a new router instance
-const router = createRouter({routeTree});
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  defaultPreloadStaleTime: 0,
+});
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -34,7 +44,9 @@ if (!rootElement.innerHTML) {
   enableMocking().then(() =>
     root.render(
       <StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </StrictMode>,
     ),
   );
