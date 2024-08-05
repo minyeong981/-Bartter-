cdc6a915abcfa16de32ebb3d63ef4cdad3eb25db
BHyeonKim <rlaqhguse@gmail.com>
2024-08-05 15:30:36 +0900

    Feat: 토큰 삽입 및 재요청 로직 추가

--- frontend/src/util/axios.ts
+++ frontend/src/util/axios.ts
@@ -1,9 +1,40 @@
 import axios from 'axios';
 
+import barter from '@/services/barter.ts';
+import useRootStore from '@/store';
+import parser from '@/util/parser.ts';
+
 const instance = axios.create({
   baseURL: 'http://localhost:8080',
   responseType: 'json',
   timeout: 4000,
 });
+
+instance.interceptors.request.use(
+  config => {
+    const token = useRootStore.getState().token;
+    config.headers.setAuthorization(`Bearer ${token}`);
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    if (error.response.status === 401) {
+      const response = await barter.reIssue();
+      const token = parser.getAccessToken(response);
+      useRootStore.getState().login(token);
+      return instance.request(error.config);
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
