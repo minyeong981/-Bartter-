1861ea0b1740f64f0a1b317a91d7c76d6dbadb54
BHyeonKim <rlaqhguse@gmail.com>
2024-08-08 13:34:59 +0900

    Feat: 물물교환 작성하기 페이지 입력값 저장

--- frontend/src/routes/_layout/trade/write/_layout/index.tsx
+++ frontend/src/routes/_layout/trade/write/_layout/index.tsx
@@ -1,5 +1,7 @@
 import {createFileRoute} from '@tanstack/react-router';
 import classnames from 'classnames/bind';
+import type {ChangeEvent} from 'react';
+import {useState} from 'react';
 
 import {ImageLettuce} from '@/assets/image';
 import GeneralButton from '@/components/Buttons/GeneralButton.tsx';
@@ -30,15 +32,43 @@ export const Route = createFileRoute('/_layout/trade/write/_layout/')({
 
 function WritePage() {
   const {cropToGive, cropsToGet} = Route.useSearch();
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [isShared, setIsShared] = useState(false);
+  const [images, setImages] = useState<File[]>([]);
 
-  console.log(cropsToGet);
+  function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
+    setTitle(e.currentTarget.value);
+  }
+
+  function handleDescriptionChange(e: ChangeEvent<HTMLTextAreaElement>) {
+    setDescription(e.currentTarget.value);
+  }
+
+  function handleCheckboxChange(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e.currentTarget.checked);
+    setIsShared(e.currentTarget.checked);
+  }
+
+  function handleImageFileChange(files: File[]) {
+    setImages(files);
+  }
 
   return (
     <div className={cx('writePage')}>
       <div className={cx('inputContainer')}>
         <CropImage imgSrc={ImageLettuce} label="상추" />
-        <LabeledInput label="제목" placeholder="제목을 입력하세요" />
-        <CheckboxInput label="나눔인가요?" />
+        <LabeledInput
+          label="제목"
+          placeholder="제목을 입력하세요"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <CheckboxInput
+          label="나눔인가요?"
+          checked={isShared}
+          onChange={handleCheckboxChange}
+        />
         <LabeledSelectCropButton
           label="주고 싶은 작물"
           selectedCrops={[cropToGive!.name]}
@@ -48,9 +78,14 @@ function WritePage() {
           selectedCrops={cropsToGet.map(crop => crop.name)}
         />
 
-        <LabeledTextAreaInput label="내용" placeholder="내용을 입력하세요" />
+        <LabeledTextAreaInput
+          label="내용"
+          placeholder="내용을 입력하세요"
+          value={description}
+          onChange={handleDescriptionChange}
+        />
         <LabeledImageInput
-          onImageChange={() => null}
+          onImageChange={handleImageFileChange}
           maxImages={3}
           label="사진"
         />
