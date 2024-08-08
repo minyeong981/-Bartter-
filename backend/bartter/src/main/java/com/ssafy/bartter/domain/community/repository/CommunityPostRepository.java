47da46b20ea5dc9fbaf488787eab05ead7698378
유지연 <apple20011215@gmail.com>
2024-08-08 09:48:38 +0900

    Fix: Param 애노테이션 import 수정

--- backend/bartter/src/main/java/com/ssafy/bartter/domain/community/repository/CommunityPostRepository.java
+++ backend/bartter/src/main/java/com/ssafy/bartter/domain/community/repository/CommunityPostRepository.java
@@ -2,10 +2,10 @@ package com.ssafy.bartter.domain.community.repository;
 
 import com.ssafy.bartter.domain.community.entity.CommunityPost;
 import com.ssafy.bartter.global.common.Location;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
