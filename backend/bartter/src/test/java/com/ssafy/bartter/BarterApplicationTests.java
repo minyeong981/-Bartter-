d2db0f56e22cab8be5691a146f5feea226c43012
garamgim <garamgim@gmail.com>
2024-08-09 11:15:43 +0900

    Chore: gradle에서 H2 삭제 및 불필요한 테스트 삭제

--- backend/bartter/src/test/java/com/ssafy/bartter/BarterApplicationTests.java
+++ backend/bartter/src/test/java/com/ssafy/bartter/BarterApplicationTests.java
@@ -1,23 +1,11 @@
 package com.ssafy.bartter;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
-@DataJpaTest
 class BartterApplicationTests {
-
-    @Autowired
-    Environment environment;
-
     @Test
     void contextLoads() {
-        Assertions.assertThat(environment.getProperty("spring.datasource.url"))
-                .isEqualTo("jdbc:h2:mem:test");
     }
 }
