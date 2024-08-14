2c70ba7d0037b9f3b5c17e8028a6acb2b6ca7df5
BHyeonKim <rlaqhguse@gmail.com>
2024-08-14 21:21:59 +0900

    Feat: 리액트 쿼리 invalidateQueries 옵션 수정

--- frontend/src/routes/_layout/_protected/community/create/index.tsx
+++ frontend/src/routes/_layout/_protected/community/create/index.tsx
@@ -1,8 +1,8 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
 import {createFileRoute, useNavigate} from '@tanstack/react-router';
 import classnames from 'classnames/bind';
 import type {ChangeEvent} from 'react';
-import { useState} from 'react';
+import {useState} from 'react';
 
 import GeneralButton from '@/components/Buttons/GeneralButton';
 import HeaderWithLabelAndBackButton from '@/components/Header/HeaderWithLabelAndBackButton';
@@ -22,17 +22,16 @@ export const Route = createFileRoute('/_layout/_protected/community/create/')({
 
 export default function PostCreate() {
   const maxImages = 3; // 허용된 최대 이미지 개수
-  
+
   const queryClient = useQueryClient();
   const nav = useNavigate({from: '/community/create'});
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [imageList, setImageList] = useState<File[]>([]);
 
-
   let cannotCreate = true;
   if (title.length > 0 && title.length < 51 && content.length > 0) {
-    cannotCreate = false
+    cannotCreate = false;
   }
 
   function handleTitleChange(event: ChangeEvent<HTMLInputElement>) {
@@ -44,37 +43,39 @@ export default function PostCreate() {
   }
 
   function handleImageChange(newImages: File[]) {
-      setImageList(newImages);
+    setImageList(newImages);
   }
 
-
   const mutation = useMutation({
-    mutationFn: ( newPost : CommunityPostForm ) => {
-      return barter.postCommunityPost(newPost)
+    mutationFn: (newPost: CommunityPostForm) => {
+      return barter.postCommunityPost(newPost);
     },
-    onSuccess: (data) => {
-      queryClient.invalidateQueries({queryKey : [querykeys.COMMUNITY_DETAIL]});
-      queryClient.invalidateQueries({queryKey : [querykeys.COMMUNITY_LIST]});
-      const postId : CommunityPostId = data.data.data.communityPostId;
-      nav({to: `/community/detail/${postId}`, replace:true})
+    onSuccess: data => {
+      queryClient.invalidateQueries({queryKey: [querykeys.COMMUNITY_DETAIL]});
+      queryClient.invalidateQueries({
+        queryKey: [querykeys.COMMUNITY_LIST],
+        refetchType: 'all',
+      });
+      const postId: CommunityPostId = data.data.data.communityPostId;
+      nav({to: `/community/detail/${postId}`, replace: true});
     },
-  })
+  });
 
   function handleSubmit() {
     if (title && content) {
-      const newPost : CommunityPostForm= {
-        title : title,
+      const newPost: CommunityPostForm = {
+        title: title,
         content: content,
-        imageList: imageList
+        imageList: imageList,
       };
-      mutation.mutate(newPost)
+      mutation.mutate(newPost);
     }
   }
 
   return (
     <div className={cx('container')}>
       <HeaderWithLabelAndBackButton label="글 작성하기" />
-    
+
       <div className={cx('main-continer')}>
         <div className={cx('form-container')}>
           <LabeledInput
@@ -90,22 +91,25 @@ export default function PostCreate() {
             value={content}
           />
           <div className={cx('image-container')}>
-          <div className={cx('image-text')}>
-            사진 ({imageList.length} / {maxImages})
+            <div className={cx('image-text')}>
+              사진 ({imageList.length} / {maxImages})
+            </div>
+            <ImageInput
+              onImageChange={handleImageChange}
+              maxImages={maxImages}
+            />
           </div>
-            <ImageInput onImageChange={handleImageChange} maxImages={maxImages} />
-          </div>        
-      </div>
+        </div>
 
-      <div className={cx('button-container')}>
-        <GeneralButton
-          buttonStyle={{style: 'primary', size: 'large'}}
-          onClick={handleSubmit}
-          disabled={cannotCreate}
-        >
-          작성완료
-        </GeneralButton>        
-      </div>
+        <div className={cx('button-container')}>
+          <GeneralButton
+            buttonStyle={{style: 'primary', size: 'large'}}
+            onClick={handleSubmit}
+            disabled={cannotCreate}
+          >
+            작성완료
+          </GeneralButton>
+        </div>
       </div>
     </div>
   );
