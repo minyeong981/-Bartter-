eed1b85b5d8883947454f5025f6e49911b10513c
BHyeonKim <rlaqhguse@gmail.com>
2024-08-15 16:17:53 +0900

    Fix: 사용하지 않는 임포트 제거

--- frontend/src/components/Inputs/CommentInput.tsx
+++ frontend/src/components/Inputs/CommentInput.tsx
@@ -1,44 +1,40 @@
-import classnames from 'classnames/bind'
+import classnames from 'classnames/bind';
 
-import GeneralButton from '../Buttons/GeneralButton';
-import styles from './CommentInput.module.scss'
+import styles from './CommentInput.module.scss';
 
 const cx = classnames.bind(styles);
 
 interface CommentInputProps {
-    content : string;
-    onChange : (event: React.ChangeEvent<HTMLInputElement>) => void;
-    onClick: (content : string) => void;
+  content: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onClick: (content: string) => void;
 }
+
 export default function CommentInput({
-    content,
-    onChange,
-    onClick
-    } : CommentInputProps ) {
+  content,
+  onChange,
+  onClick,
+}: CommentInputProps) {
+  function handleClick() {
+    onClick(content);
+  }
 
-    function handleClick() {
-        onClick(content);
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter') {
+      handleClick();
     }
+  }
 
-    function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
-        if (event.key === 'Enter') {
-            handleClick();
-        }
-    }
-
-    return (
-        <div className={cx('comment-input-container')}>
-        <input
+  return (
+    <div className={cx('comment-input-container')}>
+      <input
         type="text"
         value={content}
         onChange={onChange}
         onKeyDown={handleKeyDown}
         placeholder="댓글을 입력하세요"
       />
-      <button
-      onClick={handleClick}
-      >입력</button>
-      </div>
-    )
-
+      <button onClick={handleClick}>입력</button>
+    </div>
+  );
 }
