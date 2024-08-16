57abb6e5004445ef37f53ec965f9e141c563c485
BHyeonKim <rlaqhguse@gmail.com>
2024-08-16 09:19:18 +0900

    Chore: 데브툴 삭제

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import {createRootRoute, Outlet} from '@tanstack/react-router';
-import {TanStackRouterDevtools} from '@tanstack/router-devtools';
 import {Toaster} from 'react-hot-toast';
 
 export const Route = createRootRoute({
@@ -7,7 +6,6 @@ export const Route = createRootRoute({
     <>
       <Outlet />
       <Toaster />
-      <TanStackRouterDevtools />
     </>
   ),
 });
